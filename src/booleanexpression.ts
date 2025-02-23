672e2bc02124e0007753c3501fb4de77ac035667
Harco Kuppens <h.kuppens@gmail.com>
2025-02-23 09:57:16 +0100

    see last commit messag

--- src/booleanexpression.ts
+++ src/booleanexpression.ts
@@ -4,20 +4,56 @@ import BooleanExprParser, { ExprContext } from './generated/BooleanExprParser.js
 import { EvalVisitor } from './EvalVisitor.js';
 import { WordsVisitor } from './WordsVisitor.js';
 
-
 export default class BooleanExpression {
-    private exprContext: ExprContext;
+    private lexer: BooleanExprLexer;
+    private tokenStream: CommonTokenStream;
+    private parser: BooleanExprParser;
+    private tree: ExprContext;
+    private wordsUsedInMatch: string[];
 
     public constructor(booleanExpression: string) {
-        this.exprContext = getParserTree(booleanExpression);
+        this.lexer = new BooleanExprLexer(new CharStream(booleanExpression));
+        this.tokenStream = new CommonTokenStream(this.lexer);
+        this.parser = new BooleanExprParser(this.tokenStream);
+        this.tree = this.parser.expr();
+        // Check for errors, and if so throw exception to be handled higher up
+        if (this.parser.syntaxErrorsCount > 0) {
+            throw new SyntaxError();
+        }
+        this.wordsUsedInMatch = [];
     }
 
+    // method to log tokens, for debugging grammar
+    logTokens() {
+        let symbols: (string | null)[] = this.lexer.symbolicNames;
+        this.tokenStream.fill();
+        this.tokenStream.tokens.forEach((token) => {
+            const tokenName = symbols[token.type] || token.type;
+            console.log(`Type: ${token.type}, Name: ${tokenName}, Text: '${token.text}'`);
+        });
+    }
+
+
     getWords(): string[] {
-        return getWordsInBooleanExpr(this.exprContext);
+        const wordsVisitor = new WordsVisitor();
+        wordsVisitor.visit(this.tree);
+        return wordsVisitor.getStringValues();
     }
 
     match(text: string): boolean {
-        return matchBooleanExpr(this.exprContext, text);
+        const evalVisitor = new EvalVisitor(text);
+
+        let foundMatch = evalVisitor.visit(this.tree);
+        if (foundMatch) {
+            this.wordsUsedInMatch = evalVisitor.getStringValues();
+        } else {
+            this.wordsUsedInMatch = [];
+        }
+        return foundMatch;
+    }
+
+    getWordsUsedInLastMatch() {
+        return this.wordsUsedInMatch;
     }
 
     // directly match a boolean expression giving as string with a given text string
@@ -26,72 +62,4 @@ export default class BooleanExpression {
     static match(booleanExpression: string, text: string): boolean {
         return new BooleanExpression(booleanExpression).match(text);
     }
-}
-
-
-// private constructor(exprContext: ExprContext) {
-//     this.exprContext = exprContext;
-// }
-
-// static compile(booleanExpression: string): BooleanExpression {
-//     const exprContext = getParserTree(booleanExpression);
-//     return new BooleanExpression(exprContext);
-// }
-
-// Default export
-//export default BooleanExpression;
-
-
-//export { BooleanExpression as booleanExpression };
-
-
-//export type { ExprContext };
-
-
-
-// // method to log tokens, for debugging grammar
-// function logTokens(symbols: (string | null)[], tokenStream: CommonTokenStream) {
-//     tokenStream.fill();
-//     tokenStream.tokens.forEach((token) => {
-//         const tokenName = symbols[token.type] || token.type;
-//         console.log(`Type: ${token.type}, Name: ${tokenName}, Text: '${token.text}'`);
-//     });
-// }
-
-
-// boolexpr=booleanexpression.compile(booleanExpression: string)
-function getParserTree(booleanExpression: string): ExprContext {
-    const chars = new CharStream(booleanExpression); // replace this with a FileStream as required
-    const lexer = new BooleanExprLexer(chars);
-    const tokens = new CommonTokenStream(lexer);
-    //logTokens(lexer.symbolicNames, tokens);
-    const parser = new BooleanExprParser(tokens);
-    let tree = parser.expr();
-
-    // Check for errors, and if so throw exception to be handled higher up
-    if (parser.syntaxErrorsCount > 0) {
-        //console.error('Parsing error occurred.');
-        throw new SyntaxError
-    }
-    //console.log(tree.toStringTree(null, parser));
-    return tree;
-}
-
-// boolexpr.match
-function matchBooleanExpr(booleanExpr: ExprContext, line: string): boolean {
-    const evalVisitor = new EvalVisitor(line);
-    const foundMatch = evalVisitor.visit(booleanExpr);
-    // if (foundMatch) {
-    //     const words = evalVisitor.getStringValues();
-    //     console.log("words used in match: " + words.toString());
-    // }
-    return foundMatch;
-}
-
-// boolexpr.getWords
-function getWordsInBooleanExpr(booleanExpr: ExprContext): Array<string> {
-    const wordsVisitor = new WordsVisitor();
-    wordsVisitor.visit(booleanExpr);
-    const words = wordsVisitor.getStringValues();
-    return words;
 }
