224036e25d28524365e84537f470abdc3c616269
Harco Kuppens <h.kuppens@gmail.com>
2025-03-24 11:39:59 +0100

    fixed filename for .d.ts file in setup.bash

--- usage/using_bundled_lib/setup.bash
+++ usage/using_bundled_lib/setup.bash
@@ -6,7 +6,7 @@
 cp ../../dist_bundledlib/* .
 # cp typescript types of BooleanSearch to current folder as BooleanSearch.es.d.ts
 # This is ok, because bundled library is in ES module format and and exports typescript types of BooleanSearch class
-cp ../../types/BooleanSearch.d.ts BooleanSearch.es.d.ts
+cp ../../types/BooleanSearch.d.ts BooleanSearch.bundled.es.d.ts
 # main.ts imports /BooleanSearch.es.js
 # transpile main.ts to main.js
 tsc --target es2022 --module es2022 --moduleResolution node --sourcemap main.ts
